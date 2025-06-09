a4446c08c81d09647edb5711b7b56ff454244836
Fermin2049 <fermin2049@gmail.com>
2025-06-09 19:46:02 -0300

    integracion de Razor y ARchivos bases de vista

--- Program.cs
+++ Program.cs
@@ -1,68 +1,97 @@
-using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls ("http://127.0.0.1:5020");
-builder.Services.AddMvc ();
-builder.Services.AddControllers();
+builder.WebHost.UseUrls("http://127.0.0.1:5020");
+
+// builder.Services.AddMvc(); // No es necesario si usas AddControllersWithViews
+// builder.Services.AddControllers(); // No es necesario si usas AddControllersWithViews
+builder.Services.AddControllersWithViews(); // <-- Agrega esta línea para habilitar controladores y vistas Razor
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddApiVersioning(config => {
+builder.Services.AddApiVersioning(config =>
+{
     config.DefaultApiVersion = new ApiVersion(0, 1);
 });
-builder.Services.AddSwaggerGen(options => {
-    options.SwaggerDoc ("v0.1", new Microsoft.OpenApi.Models.OpenApiInfo {
-        Version = "v0.1",
-        Title = "Grandes Amigos",
-        Description = "API para la plataforma Grandes Amigos, una plataforma web centralizada y fácil de usar que conecta a los adultos mayores con las diversas actividades y programas ofrecidos por los distintos ministerios, fomentando la participación, la interacción social y el bienestar."
-        //Pendiente: Completar documentación OpenAPI
-    });
-    options.EnableAnnotations ();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc(
+        "v0.1",
+        new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+            Version = "v0.1",
+            Title = "Grandes Amigos",
+            Description =
+                "API para la plataforma Grandes Amigos, una plataforma web centralizada y fácil de usar que conecta a los adultos mayores con las diversas actividades y programas ofrecidos por los distintos ministerios, fomentando la participación, la interacción social y el bienestar.",
+            //Pendiente: Completar documentación OpenAPI
+        }
+    );
+    options.EnableAnnotations();
     options.DocumentFilter<ReemplazaVersion>();
     options.OperationFilter<QuitaVersion>();
 });
-builder.Services.AddDbContext<ContextoDb> (
-    options => options.UseMySql (
-        builder.Configuration ["ConnectionStrings:DefaultConnection"], new MariaDbServerVersion (new Version (10, 4, 21))
+builder.Services.AddDbContext<ContextoDb>(options =>
+    options.UseMySql(
+        builder.Configuration["ConnectionStrings:DefaultConnection"],
+        new MariaDbServerVersion(new Version(10, 4, 21))
     )
 );
 
-builder.Services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme).AddJwtBearer (options => {
-    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidIssuer = builder.Configuration ["TokenAuthentication:Issuer"],
-            ValidAudience = builder.Configuration ["TokenAuthentication:Audience"],
-            IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey (System.Text.Encoding.UTF8.GetBytes (builder.Configuration ["TokenAuthentication:SecretKey"]))
-        };
-    }
-);
-
-builder.Services.AddAuthorization (options => {
-    options.AddPolicy ("Ministerio", policy => {
-        policy.RequireRole ("Ministerio");
-        policy.RequireClaim (ClaimTypes.Role, "Ministerio");
-        policy.AddAuthenticationSchemes (JwtBearerDefaults.AuthenticationScheme);
+builder
+    .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters =
+            new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidIssuer = builder.Configuration["TokenAuthentication:Issuer"],
+                ValidAudience = builder.Configuration["TokenAuthentication:Audience"],
+                IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(
+                    System.Text.Encoding.UTF8.GetBytes(
+                        builder.Configuration["TokenAuthentication:SecretKey"]
+                    )
+                ),
+            };
     });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy(
+        "Ministerio",
+        policy =>
+        {
+            policy.RequireRole("Ministerio");
+            policy.RequireClaim(ClaimTypes.Role, "Ministerio");
+            policy.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme);
+        }
+    );
 });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment()) {
+if (!app.Environment.IsDevelopment())
+{
     //Pendiente: Manejo de errores
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-} else {
-    app.UseSwagger (options => {
-        options.RouteTemplate = "api/docs/{documentName}/docs.json";
-    }).UseSwaggerUI (options => {
-        options.SwaggerEndpoint ($"/api/docs/v0.1/docs.json", "Grandes Amigos");
-    });
+}
+else
+{
+    app.UseSwagger(options =>
+        {
+            options.RouteTemplate = "api/docs/{documentName}/docs.json";
+        })
+        .UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint($"/api/docs/v0.1/docs.json", "Grandes Amigos");
+        });
 }
 
 app.UseHttpsRedirection();
@@ -72,8 +101,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
