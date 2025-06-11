e2868e83f04588ab0d4c78586ee297c918aeb872
Fermin2049 <fermin2049@gmail.com>
2025-06-11 13:21:53 -0300

    pruebas de vistas y manejo de algunos conseptos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,18 @@
+using Grandes_Amigos.Models;
 using Microsoft.AspNetCore.Mvc;
 
 public class HomeController : Controller
 {
     public IActionResult Index()
     {
-        return View();
+        var hero = new ConfigHero
+        {
+            ImagenUrl = "/img/hero1.png", // Ajustá la ruta real
+            Titulo = "Te damos la bienvenida",
+            Subtitulo = "Plataforma para mayores",
+            TextoBoton = "Ingresar",
+        };
+
+        return View(hero);
     }
 }
---
+++ Models/ConfigHero.cs
@@ -0,0 +1,9 @@
+namespace Grandes_Amigos.Models;
+
+public class ConfigHero
+{
+    public string ImagenUrl { get; set; }
+    public string Titulo { get; set; }
+    public string Subtitulo { get; set; }
+    public string TextoBoton { get; set; }
+}
