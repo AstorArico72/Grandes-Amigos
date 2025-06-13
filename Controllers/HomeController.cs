9bd5170e04a73ccfd10e3ad06cc1e9e960c22eba
Fermin2049 <fermin2049@gmail.com>
2025-06-13 17:26:53 -0300

    incorporacion de imagenes y estructura de l apagina

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,29 @@
 using Grandes_Amigos.Models;
 using Grandes_Amigos.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 public class HomeController : Controller
 {
     private readonly INoticiaService _noticiaService;
+    private readonly ContextoDb _context;
 
-    public HomeController(INoticiaService noticiaService)
+    // Constructor que inyecta tanto el servicio de noticias como la base de datos
+    public HomeController(INoticiaService noticiaService, ContextoDb context)
     {
         _noticiaService = noticiaService;
+        _context = context;
     }
 
+    // Acción principal para la vista Index
     public async Task<IActionResult> Index()
     {
-        var model = new NoticiasViewModel
+        var model = new HomeViewModel
         {
             Deporte = await _noticiaService.GetNoticiasDeporteAsync(),
             Cultura = await _noticiaService.GetNoticiasCulturaAsync(),
             Salud = await _noticiaService.GetNoticiasSaludAsync(),
+            Eventos = await _context.Eventos.OrderBy(e => e.Fecha).ToListAsync(),
         };
 
         return View("~/Views/Public/Index.cshtml", model);
--- Models/ViewModels/HomeViewModel.cs
+++ Models/ViewModels/HomeViewModel.cs
@@ -1,8 +1,9 @@
 namespace Grandes_Amigos.Models.ViewModels;
 
-public class NoticiasViewModel
+public class HomeViewModel
 {
     public List<Noticia> Deporte { get; set; } = new List<Noticia>();
     public List<Noticia> Cultura { get; set; } = new List<Noticia>();
     public List<Noticia> Salud { get; set; } = new List<Noticia>();
+    public List<Evento> Eventos { get; set; } = new List<Evento>();
 }
