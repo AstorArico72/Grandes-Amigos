686b739a214c263d32b5428404fea31bbb8dfac7
Fermin2049 <fermin2049@gmail.com>
2025-06-15 01:10:36 -0300

    arreglos y efectos visuales

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -18,3 +18,24 @@
 		observer.observe(seccion);
 	});
 });
+
+$(document).ready(function () {
+	$('.carousel-noticias').owlCarousel({
+		loop: true,
+		margin: 16,
+		nav: true,
+		dots: false,
+		navText: [
+			'<i class="bi bi-chevron-left fs-3 text-white bg-primary rounded-circle px-2 py-1"></i>',
+			'<i class="bi bi-chevron-right fs-3 text-white bg-primary rounded-circle px-2 py-1"></i>',
+		],
+		responsive: {
+			0: {
+				items: 1,
+			},
+			768: {
+				items: 3,
+			},
+		},
+	});
+});
